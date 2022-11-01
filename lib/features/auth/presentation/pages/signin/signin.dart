3f7bf49559343cf9e5525824f9f5841b83a39e70
VershimaKelvin <kelvinityavyar@gmail.com>
2022-11-01 11:25:46 +0100

    created appbar which which can be found in appbar.png and also created input field widget which can be found in input_field.dart

---
+++ lib/features/auth/presentation/pages/signin/signin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:irish_locums/app/shared/app_bar.dart';
+import 'package:irish_locums/app/shared/input_field.dart';
+import 'package:irish_locums/core/constants/app_asset.dart';
+import 'package:irish_locums/core/constants/app_color.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({Key? key}) : super(key: key);
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
