e5baf7dcbedfb2457257a1d1b1419dd6377a67f9
VershimaKelvin <kelvinityavyar@gmail.com>
2022-11-01 14:19:03 +0100

    created signin screen

--- lib/features/auth/presentation/pages/signin/signin.dart
+++ lib/features/auth/presentation/pages/signin/signin.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:irish_locums/app/shared/app_bar.dart';
 import 'package:irish_locums/app/shared/busy_button.dart';
 import 'package:irish_locums/app/shared/input_field.dart';
-import 'package:irish_locums/app/view/app.dart';
 import 'package:irish_locums/core/constants/app_asset.dart';
 import 'package:irish_locums/core/constants/app_color.dart';
 import 'package:irish_locums/core/constants/fonts.dart';
@@ -26,7 +25,7 @@ class _SigninState extends State<Signin> {
         body:Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: SingleChildScrollView(
-            child: Container(
+            child: SizedBox(
               height: MediaQuery.of(context).size.height-MediaQuery.of(context).padding.top- MediaQuery.of(context).padding.top-44,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,6 +65,12 @@ class _SigninState extends State<Signin> {
                         placeholderColor: AppColors.borderColor,
                         suffix: SvgPicture.asset(
                             AppAssets.visibility),
+                      ),
+                      gapTiny,
+                      TextBody(
+                        'Forgot Password?',
+                        color: AppColors.yellow,
+                        fontSize: 14,
                       ),
 
                     ],
